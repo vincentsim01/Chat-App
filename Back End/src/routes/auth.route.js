4aeaeaeb2ebe9aa59b6f9e757c3e00e61122643f
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2025-01-06 22:42:43 +0800

    post

--- Back End/src/routes/auth.route.js
+++ Back End/src/routes/auth.route.js
@@ -2,10 +2,10 @@ import express from 'express';
 
 const router = express.router();
 
-router.get('/signup', (req, res) => {res.send ("signup route")})
+router.post('/signup', (req, res) => {res.send ("signup route")})
 
-router.get('/signin', (req, res) => {res.send ("signin route")})
+router.post('/signin', (req, res) => {res.send ("signin route")})
 
-router.get('/signout', (req, res) => {res.send ("signout route")})
+router.post('/signout', (req, res) => {res.send ("signout route")})
 
 export default router;
